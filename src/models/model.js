3c17864ccc6c028570c8058070b1c64b0be2249e
jgiannakos <jgiannakos@twilio.com>
2020-03-05 08:08:23 -0800

    update ignore

--- src/models/model.js
+++ src/models/model.js
@@ -3,10 +3,6 @@ const HttpException = require('../http/HttpException');
 const data = [];
 
 const create = async id => {
-  if (fetch(id)) {
-    throw new HttpException(422, 'id taken');
-  }
-
   const entity = { id };
   data.push(entity);
   return entity;
@@ -20,9 +16,12 @@ const list = async filters => {
 
 const fetch = async id => {
   const d = data.find(data => data.id === id);
+
   if (!d) {
     throw new HttpException(404, "Couldn't find");
   }
+
+  return d;
 };
 
 module.exports = {
